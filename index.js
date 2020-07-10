2875bb38129a6f9955261ddc18e53aedf422781d
geraldos <sepdwijaya18@gmail.com>
2020-07-10 10:04:43 +0700

    create first express rendering file html

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5004;
+const path = require("path");
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+//Read External File
+app.use(express.static("views"));
+
+app.get("/products", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/products.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servers runs on port ${PORT}`);
+});
